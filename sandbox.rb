9d86c2ee411801d894ba7478dfb7adf80a834278
Sara Boyer <sara.jane.boyer@gmail.com>
2020-02-15 14:54:14 -0700

    merges done

--- sandbox.rb
+++
@@ -1,9 +0,0 @@
-@slot_entries = ["@","#","$","%","^","&","*","=","+","?","!","/","~"] #13 entries
-@slot_return = Array.new
-#puts @slot_entries
-
-3.times do
-   @slot_return << @slot_entries.sample
-end
-
-puts @slot_return.join(' ')
