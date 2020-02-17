bc3c0c652c279e8f0e0124eb104d5903119e4f43
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-02-16 19:06:37 -0700

    changed ursers

--- User.rb
+++ User.rb
@@ -6,29 +6,29 @@ class User
   end
 end
 
-class User 
-  def initialize(name, wallet)
-    @name = name
-    @wallet = wallet
-  end
-​
-  def can_place_bet(amount) 
-    # returns a boolean
-    return money_in_wallet > amount
-  end
-​
-  def lost_bet(amount)
-    #logic to remove amount from wallet
-  end
-​
-  def won_bet(amount)
-    #logic to increase amount in wallet
-  end
-​
-  def money_in_wallet
-    return @wallet.quantity
-  end
-end
+# class User 
+#   def initialize(name, wallet)
+#     @name = name
+#     @wallet = wallet
+#   end
+# ​
+#   def can_place_bet(amount) 
+#     # returns a boolean
+#     return money_in_wallet > amount
+#   end
+# ​
+#   def lost_bet(amount)
+#     #logic to remove amount from wallet
+#   end
+# ​
+#   def won_bet(amount)
+#     #logic to increase amount in wallet
+#   end
+# ​
+#   def money_in_wallet
+#     return @wallet.quantity
+#   end
+# end
 
 #method to add and remove money 
 # how much money
